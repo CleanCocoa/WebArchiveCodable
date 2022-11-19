46c1ddf1285945882c5feb7eba0615c87dd07344
Christian Tietze <me@christiantietze.de>
2022-11-19 22:59:39 +0100

    decode MIME types

--- Sources/WebArchiveCodable/WebArchiveCodable.swift
+++ Sources/WebArchiveCodable/WebArchiveCodable.swift
@@ -1,5 +1,38 @@
 //  Copyright © 2022 Christian Tietze. All rights reserved. Distributed under the MIT License.
 
-public struct WebArchive: Codable {
-    
+public struct WebArchive: Equatable {
+    public struct WebResource: Equatable {
+        public let MIMEType: String
+    }
+
+    public let mainResource: WebResource
+    public let subresources: [WebResource]?
+}
+
+extension WebArchive.WebResource: Decodable {
+    enum CodingKeys: String, CodingKey {
+        case MIMEType = "WebResourceMIMEType"
+    }
+
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        try self.init(
+            MIMEType: container.decode(String.self, forKey: .MIMEType)
+        )
+    }
+}
+
+extension WebArchive: Decodable {
+    enum CodingKeys: String, CodingKey {
+        case mainResource = "WebMainResource"
+        case subresources = "WebSubresources"
+    }
+
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        try self.init(
+            mainResource: container.decode(WebResource.self, forKey: .mainResource),
+            subresources: container.decode(Optional<Array<WebResource>>.self, forKey: .subresources)
+        )
+    }
 }
--- Tests/WebArchiveCodableTests/WebArchiveCodableTests.swift
+++ Tests/WebArchiveCodableTests/WebArchiveCodableTests.swift
@@ -2,4 +2,15 @@ import XCTest
 @testable import WebArchiveCodable
 
 final class WebArchiveCodableTests: XCTestCase {
+    func test_AdeleFixture() throws {
+        let url = try XCTUnwrap(Bundle.module.url(forResource: "adele-for-vogue-in-2021", withExtension: "webarchive", subdirectory: "Fixtures"))
+        let data = try Data(contentsOf: url)
+        let webArchive = try PropertyListDecoder().decode(WebArchive.self, from: data)
+
+        XCTAssertEqual(webArchive, WebArchive(
+            mainResource: .init(MIMEType: "text/html"),
+            subresources: [
+                .init(MIMEType: "image/png"),
+            ]))
+    }
 }
