39fdf40a78cac4f5b568badd6cc87fdc28fec5ca
Christian Tietze <me@christiantietze.de>
2022-11-19 23:47:03 +0100

    document types according to WebKit docs

--- Sources/WebArchiveCodable/WebArchiveCodable.swift
+++ Sources/WebArchiveCodable/WebArchiveCodable.swift
@@ -1,39 +1,65 @@
 import Foundation
 
+/// Represents a web site that can be archived.
+///
+/// Web content is archived for example on disk when saving web pages from Safari, and on the pasteboard when copying data.
+///
+/// The ``mainResource`` can be, for example,
+/// - an entire page,
+/// - a portion of a web page,
+/// - other data like an image.
+///
+/// For the legacy Objective-C version, see <https://developer.apple.com/documentation/webkit/webarchive> which was deprecated for macOS 10.14.
+///
+/// ## Codable content
+///
+/// Use `PropertyListDecoder` to decode `.webarchive` files or `Data` from a `NSPasteboard` with the `"Apple Web Archive pasteboard type"` or "`com.apple.webarchive"` pasteboard types.
 public struct WebArchive {
-    /// Represents a downloaded URL.
+    /// Represents a downloaded resource.
     ///
     /// A ``Resource`` encapsulates the data of the download as well as associated resource properties such as the URL, MIME type, and frame name.
     ///
     /// Confer WebKit's `WebResource.h`:  <https://github.com/WebKit/WebKit/blob/main/Source/WebKitLegacy/win/WebResource.h>
+    ///
+    /// For the Objective-C version, see <https://developer.apple.com/documentation/webkit/webresource> -- which notably was not deprecated with `WebArchive`.
     public struct Resource {
+        /// The downloaded data.
+        public let data: Data?
+
+        /// The downloaded resource's orignal URL.
         public let url: URL?
 
+        /// The downloaded resource's MIME type.
         public let mimeType: String
 
-        public let data: Data?
+        /// The name of the frame.
+        ///
+        /// If the receiver does not represent the contents of an entire HTML frame, returns `nil`.
         public let frameName: String?
     }
 
+    /// The receiver's main resource.
     public let mainResource: Resource
+
+    /// The receiver's subresources, or `nil` if there are none.
     public let subresources: [Resource]?
 }
 
 extension WebArchive.Resource: Decodable {
     enum CodingKeys: String, CodingKey {
+        case data = "WebResourceData"
         case url = "WebResourceURL"
         case MIMEType = "WebResourceMIMEType"
-        case data = "WebResourceData"
         case frameName = "WebResourceFrameName"
     }
 
     public init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
         try self.init(
+            data: container.decodeIfPresent(Data.self, forKey: .data),
             url: container.decodeIfPresent(String.self, forKey: .url)
                     .flatMap(URL.init(string:)),
             mimeType: container.decode(String.self, forKey: .MIMEType),
-            data: container.decodeIfPresent(Data.self, forKey: .data),
             frameName: container.decodeIfPresent(String.self, forKey: .frameName)
         )
     }
